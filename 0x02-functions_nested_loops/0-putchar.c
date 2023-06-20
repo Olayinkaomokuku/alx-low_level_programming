e608fe9db3309448369151ab611791191883852c
Olayinka Kuku <horlarbrayne@gmail.com>
2023-06-20 12:27:10 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
-	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
-	 */
-
+/**
+* main - print 0-putchar.c
+* is written by mbah
+* Return: 0
+*/
 
 	int main(void)
 	{
